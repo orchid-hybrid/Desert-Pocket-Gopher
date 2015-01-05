1cc25034323abdecc5e4af7ab7f19212dbc0a9e8
tca <theconartist@gmail.com>
2015-01-04 23:18:08 -0600

    some cleanup and comments

--- example.ml
+++ example.ml
@@ -24,32 +24,34 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) (FS:KV_RO) = struct
     try_bind
       (fun () -> Channel.read_line chan)
       (function
-        | [] ->  report_and_close c chan "Echo connection closure initiated."
-        | bufs -> site (Cstruct.copyv bufs)
-	          >>= fun () ->
-                  Channel.flush chan
-                  >>= fun () ->
+        | [] ->  report_and_close c chan "Client EOF"
+        | bufs -> site (Cstruct.copyv bufs) >>= fun () ->
                   Channel.close chan)
-      (fun error -> report_and_close c chan "Echo connection closure initiated.")
+      (fun error -> report_and_close c chan "ERROR: connection closed.")
 
+  (* set of IO functions specialized to the connection for handlers to use *)
   let make_gio chan fs host port =
     object
-      method send_file name = read_fs fs name >>= (fun bufs -> Channel.write_line chan bufs; return ())
+      method read_line () = Channel.read_line chan
       method write_line s = Channel.write_line chan s
+      method send_file name = read_fs fs name >>= (fun bufs -> Channel.write_line chan bufs; return ())
       method list_file n f = Channel.write_line chan (Printf.sprintf "%s\t%s\t%s\t%s\n" n f host port)
     end;;
 
   let start c s fs =
-    let ip = String.concat ", " (List.map Ipaddr.V4.to_string (S.IPV4.get_ip (S.ipv4 s))) in
-    let start_gopher flow ic oc =
+    let ip = String.concat ", " (List.map Ipaddr.V4.to_string (S.IPV4.get_ip (S.ipv4 s)))
+
+    (* generate table of ("path" -> handler) for pages on the site, from DSL *)
+    and dispatch_table = Dsl.make_site (Site.site ()) in
+
+    let handle_connection flow ic oc =
       let chan = Channel.create flow in
-      let dispatch_table = Dsl.make_site (Site.site ()) in
       let gio = make_gio chan fs "10.0.0.2" "70" in
       let handler req = (Hashtbl.find dispatch_table req) gio in
       gopher c chan handler in
-    C.log_s c (Printf.sprintf "IP address: %s\n" ip)
-    >>= fun () ->
+
+    C.log_s c (Printf.sprintf "IP address: %s\n" ip) >>= fun () ->
     CON.init ~stack:s () >>= fun ctx ->
-    CON.serve ~ctx ~mode:(`TCP (`Port 70)) start_gopher
-              
+    CON.serve ~ctx ~mode:(`TCP (`Port 70)) handle_connection
+
 end
